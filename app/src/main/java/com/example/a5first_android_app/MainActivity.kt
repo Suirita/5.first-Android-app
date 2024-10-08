e561e26f0e124c9c7df79425890b4b119c073c05
Suirita Fahd <suirita.fahd.solicode@gmail.com>
2024-10-08 22:37:44 +0100

    padding

--- app/src/main/java/com/example/a5first_android_app/MainActivity.kt
+++ app/src/main/java/com/example/a5first_android_app/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -14,6 +13,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.a5first_android_app.ui.theme._5firstAndroidappTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.padding
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,10 +35,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Gray) {
+    Surface(color = Color.Cyan) {
         Text(
-            text = "Hello, my name is $name!",
-            modifier = modifier
+            text = "Hi, my name is $name!",
+            modifier = modifier.padding(24.dp)
         )
     }
 }
