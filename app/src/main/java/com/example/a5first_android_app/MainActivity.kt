840db94946c8d0762650c9ff92aa743497aed40a
Suirita Fahd <suirita.fahd.solicode@gmail.com>
2024-10-08 16:58:16 +0100

    background color

---
+++ app/src/main/java/com/example/a5first_android_app/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.a5first_android_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.a5first_android_app.ui.theme._5firstAndroidappTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            _5firstAndroidappTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(color = Color.Gray) {
+        Text(
+            text = "Hello, my name is $name!",
+            modifier = modifier
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    _5firstAndroidappTheme {
+        Greeting("fahd")
+    }
+}
